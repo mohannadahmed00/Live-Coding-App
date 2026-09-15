95818e6f58e8d271a46a267e59c286886b9c2e20
Mohannad Ahmed <mohannad199344@gmail.com>
2026-09-15 09:31:31 +0300

    Remove redundant Compose BOM from app androidTest dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core)
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
